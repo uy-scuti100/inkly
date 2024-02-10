297415bf51550dbf01ab22f9140bbefe88a51a4c
Hussain Yusuf <114601628+uy-scuti100@users.noreply.github.com>
2024-02-10 02:27:16 +0100

    fix: fixing the hero video background

--- src/components/homepage/Herosection.jsx
+++ src/components/homepage/Herosection.jsx
@@ -7,7 +7,7 @@ export default function Herosection() {
       <Navbar />
 
       <div className="absolute inset-0 bg-[#8099eb87] opacity-50" />
-      <video muted loop autoPlay className="relative">
+      <video src="/hero.mp4" muted loop autoPlay className="relative">
         <source src="/hero.mp4" type="video/mp4" />
       </video>
     </section>
