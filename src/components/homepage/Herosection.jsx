97d3d23f5176f7a6b8ed0bb455ba8256bb698d67
Hussain Yusuf <114601628+uy-scuti100@users.noreply.github.com>
2024-02-10 01:34:40 +0100

    changed the video fram a source of 23mb to a 2,2 mb alternative

--- src/components/homepage/Herosection.jsx
+++ src/components/homepage/Herosection.jsx
@@ -8,7 +8,7 @@ export default function Herosection() {
 
       <div className="absolute inset-0 bg-[#8099eb87] opacity-50" />
       <video muted loop autoPlay className="relative">
-        <source src="/0123.mp4" type="video/mp4" />
+        <source src="/hero.mp4" type="video/mp4" />
       </video>
     </section>
   );
